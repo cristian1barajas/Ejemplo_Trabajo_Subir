a983e6eed8c36d292b6843489dc67149a68e935f
cristian1barajas <cristian1barajas@hotmail.com>
2021-02-24 20:46:18 -0500

    Versión Inicial del programa
    Ejemplo de como hacer un commit

---
+++ ejercicio1.cpp
@@ -0,0 +1,32 @@
+/*1. Escribe un programa que lea de la entrada estándar dos números y muestre 
+en la salida estándar su suma, resta, multiplicación y división.*/
+
+#include <Arduino.h>
+
+float suma = 0, resta = 0, multiplicacion=0;
+float n1, n2;
+float division = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  Serial.println("Digite un numero: "); while(!Serial.available()){} 
+  n1 = Serial.parseInt(); Serial.println(String(n1));
+
+  Serial.println("Digite otro numero: "); while(!Serial.available()){} 
+  n2 = Serial.parseInt(); Serial.println(String(n2));
+  
+  suma = n1 + n2;
+	resta = n1 - n2;
+	multiplicacion = n1 * n2;
+	division = n1 / n2;
+
+  Serial.println("La suma es: " + String(suma));
+  Serial.println("La resta es: " + String(resta));
+  Serial.println("La multiplicación es: " + String(multiplicacion));
+  Serial.println("La división es: " + String(division));
+
+  while(true){}
+}
